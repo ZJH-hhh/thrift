a972f01719aef977a1feea7d0f5a7c60d885e5a9
zjh <494731164@qq.com>
2022-07-21 18:15:27 +0800

    update match_client

--- game/src/client.py
+++ game/src/client.py
@@ -6,8 +6,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
+
+def operate(op, user_id, user_name, sorce):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -23,11 +25,21 @@ def main():
     # Connect!
     transport.open()
 
-    user = User(1, "zjh", 1200)
-    client.add_user(user, "")
+    user = User(user_id, user_name, sorce)
+
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
 
     # Close!
     transport.close()
+
+
+def main():
+    for line in stdin:
+        op, user_id, user_name, score = line.split(' ')
+        operate(op, int(user_id), user_name, int(score))
 
 
 if __name__ == "__main__":
